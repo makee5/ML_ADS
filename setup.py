135f79f3ca63a20b62e2c07d336addad3e797de8
João Correia <jfscorreia95@gmail.com>
2024-02-26 14:28:03 +0000

    [UPDATE] setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ml_from_scratch',
+    name='ml_ads',
     version='0.0.1',
     python_requires='>=3.10',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     zip_safe=False,
     install_requires=['numpy', 'pandas', 'scipy'],
-    author='João Correia',
-    author_email='jfscorreia95@gmail.com',
-    description='Machine Learning - Applied Data Science',
+    author='',
+    author_email='',
+    description='Machine Learning - Applied Data Science 2024',
     license='MIT',
     keywords='',
 )
